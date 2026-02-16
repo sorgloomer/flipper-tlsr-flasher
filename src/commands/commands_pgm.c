c3e5e0d4f43a4b0d479e82f4f8ecfca8555b497e
Tamas Hegedus <sorgloomer@gmail.com>
2026-02-16 22:42:08 +0100

    improve arg parsing

--- src/commands/commands_pgm.c
+++ src/commands/commands_pgm.c
@@ -6,6 +6,48 @@
 #include "src/swire/swire_bitbang.h"
 
 #define _OK_RESPONSES 0
+
+bool cmd_matches(FuriString* input, const char* cmd);
+const char* cmd_get_params(FuriString* cmd);
+void cmd_pgm_init(SwireApp* app, const char* params);
+void cmd_pgm_transaction_start(SwireApp* app, const char* params);
+void cmd_pgm_transaction_end(SwireApp* app, const char* params);
+FuriStatus cmd_pgm_bytes_read(SwireApp* app, const char* params);
+FuriStatus cmd_pgm_bytes_write(SwireApp* app, const char* params);
+FuriStatus cmd_pgm_reset(SwireApp* app, const char* params);
+
+static SwireBitbang* cmd_swire_alloc(SwireApp* app);
+
+bool cmd_pgm(SwireApp* app, FuriString* cmd) {
+    const char* cargs = cmd_get_params(cmd);
+
+    if(cmd_matches(cmd, "swire_init")) {
+        cmd_pgm_init(app, cargs);
+        return true;
+    }
+    if(cmd_matches(cmd, "trs")) {
+        cmd_pgm_transaction_start(app, cargs);
+        return true;
+    }
+    if(cmd_matches(cmd, "tre")) {
+        cmd_pgm_transaction_end(app, cargs);
+        return true;
+    }
+    if(cmd_matches(cmd, "bw")) {
+        cmd_pgm_bytes_write(app, cargs);
+        return true;
+    }
+    if(cmd_matches(cmd, "br")) {
+        cmd_pgm_bytes_read(app, cargs);
+        return true;
+    }
+    if(cmd_matches(cmd, "reset")) {
+        cmd_pgm_reset(app, cargs);
+        return true;
+    }
+    return false;
+}
+
 static SwireBitbang* cmd_swire_alloc(SwireApp* app) {
     SwireBitbang* swire = swire_bitbang_alloc_with_sws((IoPins){
         .in = &gpio_ext_pa7,
@@ -15,17 +57,16 @@ static SwireBitbang* cmd_swire_alloc(SwireApp* app) {
     return swire;
 }
 
-void cmd_pgm_init(SwireApp* app, const char* params) {
+void cmd_pgm_init(SwireApp* app, const char* cargs) {
     if(app->usb == NULL) {
         FURI_LOG_E("swire", "cmd_pgm_transaction_start swire->usb not initialized");
         return;
     }
-    const char* rest = strchr(params, ' ');
-    if(rest == NULL) {
+    if(cargs == NULL) {
         swire_usb_printf_line(app->usb, "error no params");
         return;
     }
-    int matched = sscanf(rest, "%ld %ld", &app->config->addrsize, &app->config->bitrate);
+    int matched = sscanf(cargs, "%ld %ld", &app->config->addrsize, &app->config->bitrate);
     if(matched != 2) {
         swire_usb_printf_line(app->usb, "error params %d", matched);
         return;
@@ -39,18 +80,17 @@ void cmd_pgm_init(SwireApp* app, const char* params) {
 #endif
 }
 
-void cmd_pgm_transaction_start(SwireApp* app, const char* params) {
+void cmd_pgm_transaction_start(SwireApp* app, const char* cargs) {
     if(app->usb == NULL) {
         FURI_LOG_E("swire", "cmd_pgm_transaction_start swire->usb not initialized");
         return;
     }
-    const char* rest = strchr(params, ' ');
-    if(rest == NULL) {
+    if(cargs == NULL) {
         swire_usb_printf_line(app->usb, "error no params");
         return;
     }
     int32_t addr, wr, slaveid;
-    int matched = sscanf(rest, "%lx %lx %lx", &addr, &wr, &slaveid);
+    int matched = sscanf(cargs, "%lx %lx %lx", &addr, &wr, &slaveid);
     if(matched != 3) {
         swire_usb_printf_line(app->usb, "error params %d");
         return;
@@ -70,7 +110,8 @@ void cmd_pgm_transaction_start(SwireApp* app, const char* params) {
 #endif
 }
 
-void cmd_pgm_transaction_end(SwireApp* app) {
+void cmd_pgm_transaction_end(SwireApp* app, const char* cargs) {
+    UNUSED(cargs);
     if(app->usb == NULL) {
         FURI_LOG_E("swire", "cmd_pgm_transaction_end swire->usb not initialized");
         return;
@@ -88,19 +129,18 @@ void cmd_pgm_transaction_end(SwireApp* app) {
 #endif
 }
 
-FuriStatus cmd_pgm_bytes_write(SwireApp* app, const char* params) {
+FuriStatus cmd_pgm_bytes_write(SwireApp* app, const char* cargs) {
     if(app->usb == NULL) {
         FURI_LOG_E("swire", "cmd_pgm_bytes_write swire->usb not initialized");
         return FuriStatusErrorResource;
     }
     FuriStatus status;
-    const char* rest = strchr(params, ' ');
-    if(rest == NULL) {
+    if(cargs == NULL) {
         swire_usb_printf_line(app->usb, "error no params");
         return FuriStatusErrorParameter;
     }
     int32_t bytecount;
-    int matched = sscanf(rest, "%lx", &bytecount);
+    int matched = sscanf(cargs, "%lx", &bytecount);
     if(matched != 1) {
         swire_usb_printf_line(app->usb, "error params matched %d", matched);
         return FuriStatusErrorParameter;
@@ -142,19 +182,18 @@ FuriStatus cmd_pgm_bytes_write(SwireApp* app, const char* params) {
     return FuriStatusOk;
 }
 
-FuriStatus cmd_pgm_bytes_read(SwireApp* app, const char* params) {
+FuriStatus cmd_pgm_bytes_read(SwireApp* app, const char* cargs) {
     if(app->usb == NULL) {
         FURI_LOG_E("swire", "cmd_pgm_bytes_read swire->usb not initialized");
         return FuriStatusErrorResource;
     }
     FuriStatus status;
-    const char* rest = strchr(params, ' ');
-    if(rest == NULL) {
+    if(cargs == NULL) {
         swire_usb_printf_line(app->usb, "error no params");
         return FuriStatusErrorParameter;
     }
     int32_t bytecount;
-    int matched = sscanf(rest, "%lx", &bytecount);
+    int matched = sscanf(cargs, "%lx", &bytecount);
     if(matched != 1) {
         swire_usb_printf_line(app->usb, "error params matched %d", matched);
         return FuriStatusErrorParameter;
@@ -197,48 +236,46 @@ FuriStatus cmd_pgm_bytes_read(SwireApp* app, const char* params) {
     return FuriStatusOk;
 }
 
-FuriStatus cmd_pgm_reset(SwireApp* app) {
+FuriStatus cmd_pgm_reset(SwireApp* app, const char* cargs) {
     if(app->usb == NULL) {
         FURI_LOG_E("swire", "cmd_pgm_reset swire->usb not initialized");
         return FuriStatusErrorResource;
     }
+    int32_t reset_duration_ms = 0;
+    int32_t reset_delay_ms = 0;
+    sscanf(cargs, "%ld %ld", &reset_delay_ms, &reset_duration_ms);
+    if(reset_duration_ms == 0) {
+        reset_duration_ms = app->config->reset_duration_ms;
+    }
+    if(reset_delay_ms == 0) {
+        reset_delay_ms = app->config->reset_delay_ms;
+    }
+
     const GpioPin* pin_power = &gpio_ext_pb2;
     furi_hal_gpio_init_simple(pin_power, GpioModeOutputPushPull);
     furi_hal_gpio_write(pin_power, false);
-    furi_delay_ms(app->config->reset_duration_ms);
+    furi_delay_ms(reset_duration_ms);
     furi_hal_gpio_write(pin_power, true);
-    furi_delay_ms(app->config->reset_delay_ms);
-    swire_usb_printf_line(
-        app->usb, "# %ld %ld", app->config->reset_duration_ms, app->config->reset_delay_ms);
+    furi_delay_ms(reset_delay_ms);
+    swire_usb_printf_line(app->usb, "# reset %ld %ld", reset_delay_ms, reset_duration_ms);
     swire_usb_writeline_cstr(app->usb, "ok");
     return FuriStatusOk;
 }
 
-bool cmd_pgm(SwireApp* app, FuriString* cmd) {
-    if(furi_string_start_with(cmd, "swire_init ")) {
-        cmd_pgm_init(app, furi_string_get_cstr(cmd));
+bool cmd_matches(FuriString* input, const char* cmd) {
+    if(!furi_string_start_with(input, cmd)) {
+        return false;
+    }
+    unsigned int cmdlen = strlen(cmd);
+    if(furi_string_size(input) == cmdlen) {
         return true;
     }
-
-    if(furi_string_start_with(cmd, "trs ")) {
-        cmd_pgm_transaction_start(app, furi_string_get_cstr(cmd));
-        return true;
-    }
-    if(furi_string_equal(cmd, "tre")) {
-        cmd_pgm_transaction_end(app);
-        return true;
-    }
-    if(furi_string_start_with(cmd, "bw ")) {
-        cmd_pgm_bytes_write(app, furi_string_get_cstr(cmd));
-        return true;
-    }
-    if(furi_string_start_with(cmd, "br ")) {
-        cmd_pgm_bytes_read(app, furi_string_get_cstr(cmd));
-        return true;
-    }
-    if(furi_string_equal(cmd, "reset")) {
-        cmd_pgm_reset(app);
+    if(furi_string_size(input) > cmdlen && furi_string_get_char(input, cmdlen) == ' ') {
         return true;
     }
     return false;
+}
+const char* cmd_get_params(FuriString* cmd) {
+    const char* ccmd = furi_string_get_cstr(cmd);
+    return strchr(ccmd, ' ');
 }
